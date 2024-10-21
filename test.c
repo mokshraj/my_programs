0d92988e42a71309f324b6ccfecf43228ce97a0b
mokshraj <mokshraj20@gmail.com>
2024-10-21 09:03:14 +0530

    test program update

--- test.c
+++ test.c
@@ -1,22 +1,15 @@
 #include <stdio.h>
 int main()
 {
-   int a,b,c,d,f=0;
-   a = 153;
-   c= a;
-   while(c!=0){
-    c/=10;
-    b++;
-   }
-   c=a;
-   while(c!=0){
-    d= c%10;
-    c/=10;
-    int i;
-    int e=1;
-    for(i=1;i<=b;i++){
-        e = e*d;
+    int n =10,a=0,b=1,c;
+    printf("%d %d",a,b);
+    while(n!=0)
+    {
+        c=a+b;
+        printf(" %d",c);
+        a=b;
+        b=c;
+        n--;
     }
-    f= f+e;
-   }
-   printf("%d",f);}
+    return 0;
+}
