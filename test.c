96edb93dbf380474f8a67f81bb181d7c61fdca03
mokshraj <mokshraj20@gmail.com>
2024-11-22 00:37:28 +0530

    string test

--- test.c
+++ test.c
@@ -1,12 +1,49 @@
+// #include <stdio.h>
+// #include <string.h>
+// int main(){
+//     char name[20];
+//     printf("Enter a string : ");
+//     scanf("%s",&name);
+//     int i,a= strlen(name);
+//     for(i=0;i<a;i++){
+//         printf("%d\n",name[i]);
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+// #include <string.h>
+// int main(){
+//     char str[6],str2[6]="shita";
+//     printf("before strcpy str1: %s str2: %s \n",str,str2);
+//     strcpy(str,str2);
+//     printf("after strcpy str1: %s str2: %s \n",str,str2);
+//     printf("No. of characters in the string : %d",strlen(str2));
+//     return 0;
+// }
+
+// #include <stdio.h>
+// #include <string.h>
+// int main(){
+//     char str[4],str1[4],str2[4];
+//     int i;
+//     scanf("%3s",&str);
+//     strcpy(str2,str);
+//     for(i=strlen(str);i<-1;i--){
+//         strcat(str1,str);
+//     }
+//     printf("%s %s",str,str1);
+//     if(str2 == str1){
+//         printf("it's a palendrome");
+//     }
+//     else{
+//         printf("it's not a palendrome");
+//     }
+//     return 0;
+// }
+
 #include <stdio.h>
-int main()
-{
-    int a=0,i,array[5]={1,2,3,4,5};
-    for(i=0;i<5;i++){
-        if(array[i] > array[i+1]){
-            a = array[i];
-        }
-    }
-    printf("%d",a);
-    return 0;
+#include <string.h>
+int main(){
+    
 }
