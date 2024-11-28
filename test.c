4a4f1076dcface63bc7824f8c8fc54b8fb25f26b
mokshraj <mokshraj20@gmail.com>
2024-11-28 12:01:43 +0530

    added palendrome test

--- test.c
+++ test.c
@@ -22,25 +22,25 @@
 //     return 0;
 // }
 
-#include <stdio.h>
-#include <string.h>
-int main(){
-    char str[4],str1[4],temp[2];
-    int i;
-    printf("Enter a 3 alphabet palendrome : ");
-    scanf("%3s",&str);
-    for(i=strlen(str);i>=0;i--){
-        temp[0]=str[i];temp[1]='\0';
-        strcat(str1,temp);
-    }
-    if(strcmp(str,str1)==0){
-        printf("it's a palendrome");
-    }
-    else{
-        printf("it's not a palendrome");
-    }
-    return 0;
-}
+// #include <stdio.h>
+// #include <string.h>
+// int main(){
+//     char str[4],str1[4],temp[2];
+//     int i;
+//     printf("Enter a 3 alphabet palendrome : ");
+//     scanf("%3s",&str);
+//     for(i=strlen(str);i>=0;i--){
+//         temp[0]=str[i];temp[1]='\0';
+//         strcat(str1,temp);
+//     }
+//     if(strcmp(str,str1)==0){
+//         printf("it's a palendrome");
+//     }
+//     else{
+//         printf("it's not a palendrome");
+//     }
+//     return 0;
+// }
 
 // #include <stdio.h>
 // #include <string.h>
