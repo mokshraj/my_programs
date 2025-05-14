715cffac20289016abcfe9873addf055b6b6ca40
baymax <mokshraj20@gmail.com>
2025-05-14 11:19:52 +0530

    C lab update (commandline arguments update)

--- dsa.c
+++ dsa.c
@@ -1518,5 +1518,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(int argc, char *argv[]){
-    printf("Sum = %d",atoi(argv[1])+atoi(argv[2]));
+    int i,sum =0;
+    if(argc<2){
+        printf("too few argumnets");
+    }
+    else{
+        for(i=1;i<argc;i++){
+            sum = sum + atoi(argv[i]);
+        }
+        printf("Sum = %d",sum);
+    }
 }
