eba5ddd3183a912f780f39a3a1d5da6881d6b57c
mokshraj <mokshraj20@gmail.com>
2025-09-03 10:18:22 +0530

    updated cir-queue

--- dslab2.c
+++ dslab2.c
@@ -362,73 +362,296 @@
 // 	return 0;
 // }
 
-//16. write a menu driver program for stack push pop display peek
-#include<stdio.h>
+// //16. write a menu driver program for stack push pop display peek
+// #include<stdio.h>
+// #include <stdlib.h>
+// #define MAX 10
+// void PUSH(int *stack, int *top){
+// 	if(*top != MAX-1){
+// 		int n;
+// 		scanf("%d",&n);
+// 		stack[++(*top)] = n;
+// 	}
+// 	else{
+// 		perror("\nMAX top size reached\n");
+// 	}
+// }
+// void POP(int *stack,int *top){
+// 	if(*top != -1){
+// 		(*top)--;
+// 	}
+// 	else{
+// 		perror("\nStack is empty\n");
+// 	}
+// }
+// void display(int stack[] ,int top){
+// 	printf("\n");
+// 	while(top!=-1){
+// 		printf("%d\n",stack[top]);
+// 		top--;
+// 	}
+// 	printf("\n\n");
+// }
+// void peek(int stack[],int top){
+// 	printf("\n%d\n\n",stack[top]);
+// }
+// int main(){
+// 	int top = -1;
+// 	char ch;
+// 	int stack[MAX];
+// 	int choice;
+// 	while (1){
+// 		printf("1.PUSH\n2.POP\n3.display\n4.peek\n5.exit\n");
+// 		printf("Enter a choice : ");
+// 		scanf("%d",&choice);
+// 		switch (choice)
+// 		{
+// 		case 1:
+// 			PUSH(stack,&top);
+// 			break;
+// 		case 2:
+// 			POP(stack,&top);
+// 			break;
+// 		case 3:
+// 			display(stack,top);
+// 			break;
+// 		case 4:
+// 			peek(stack,top);
+// 			break;
+// 		case 5:
+// 			exit(0);
+// 			break;
+// 		default:
+// 			printf("Invalid input");
+// 			break;
+// 		}
+// 		while (getchar() != '\n') {}
+// 		printf("\npress enter to continue.................. ");
+// 		while (getchar() != '\n') {}
+// 		system("clear");
+// 	}
+// 	return 0;
+// }
+
+// //17.write a menu driven program to implement a simple queue with basic functions
+// #include <stdio.h>
+// #include <stdlib.h>
+// #define MAX 10
+// typedef struct {
+//     int queue[MAX];
+//     int front;
+//     int rear;
+// } queue;
+// int isFULL(queue *Q) {
+//     return Q->rear == MAX - 1;
+// }
+// int isEMPTY(queue *Q) {
+//     return Q->front == Q->rear;
+// }
+// void enqueue(queue *Q, int n) {
+//     if (isFULL(Q)) {
+//         printf("Queue is full!\n");
+//         return;
+//     }
+//     Q->rear++;
+//     Q->queue[Q->rear] = n;
+//     printf("%d enqueued to queue\n", n);
+// }
+// void dequeue(queue *Q) {
+//     if (isEMPTY(Q)) {
+//         printf("Queue is empty!\n");
+//         return;
+//     }
+//     Q->front++;
+//     printf("Dequeued %d from queue\n", Q->queue[Q->front]);
+// }
+// void peek(queue *Q) {
+//     if (isEMPTY(Q)) {
+//         printf("Queue is empty!\n");
+//         return;
+//     }
+//     printf("%d is at the front (peek)\n", Q->queue[Q->front + 1]);
+// }
+// void display(queue *Q) {
+//     if (isEMPTY(Q)) {
+//         printf("Queue is empty!\n");
+//         return;
+//     }
+//     printf("Queue elements: ");
+//     for (int i = Q->front + 1; i <= Q->rear; i++) {
+//         printf("%d ", Q->queue[i]);
+//     }
+//     printf("\n");
+// }
+// int main() {
+//     queue Q;
+//     Q.front = -1;
+//     Q.rear = -1;
+//     int choice, n;
+//     while (1) {
+//         printf("\n1. Display\n2. Peek\n3. Enqueue\n4. Dequeue\n5. Exit\nEnter your choice: ");
+//         scanf("%d", &choice);
+//         switch (choice) {
+//             case 1:
+//                 display(&Q);
+//                 break;
+//             case 2:
+//                 peek(&Q);
+//                 break;
+//             case 3:
+//                 printf("Enter value to enqueue: ");
+//                 scanf("%d", &n);
+//                 enqueue(&Q, n);
+//                 break;
+//             case 4:
+//                 dequeue(&Q);
+//                 break;
+//             case 5:
+//                 exit(0);
+//             default:
+//                 printf("Invalid input\n");
+//         }
+//         while (getchar() != '\n') {}
+//         printf("Press enter to continue...");
+//         while (getchar() != '\n') {}
+//         system("clear");
+//     }
+//     return 0;
+// }
+
+
+// //18.write a menu driven program to implement a circular queue  basic with basic functions
+// #include <stdio.h>
+// #define MAX 5
+// typedef struct{
+//     int queue[MAX];
+//     int front;
+//     int rear;
+//     int current;
+// }queue;
+
+// void enqueue(queue *Q, int n) {
+//     if (Q->current < MAX) {
+//         Q->rear = (Q->rear + 1) % MAX;
+//         Q->queue[Q->rear] = n;
+//         Q->current++;
+//     }
+//     else {
+//         perror("queue is full");
+//     }
+// }
+
+// int dequeue(queue *Q) {
+//     if (Q->current == 0) {
+//         perror("queue is empty");
+//         return -1;
+//     }
+//     else {
+//         int temp = Q->queue[Q->front];
+//         Q->front = (Q->front + 1) % MAX;
+//         Q->current--;
+//         return temp;
+//     }
+// }
+
+// void display(queue Q) {
+//     int index = (Q.front + 1) % MAX;
+//     for (int i = 0; i < Q.current; i++) {
+//         printf("%d\t", Q.queue[index]);
+//         index = (index + 1) % MAX;
+//     }
+//     printf("\n");
+// }
+
+// void peek(queue Q) {
+//     if (Q.current == 0) {
+//         printf("Queue is empty\n");
+//         return;
+//     }
+//     int index = (Q.front + 1) % MAX;
+//     printf("%d\n", Q.queue[index]);
+// }
+
+// void queinit(queue *Q) {
+//     Q->front = -1;
+//     Q->rear = -1;
+//     Q->current = 0;
+// }
+
+// int main()
+// {
+//     queue Q;
+//     queinit(&Q);
+//     enqueue(&Q,7);
+//     printf("\n");
+//     display(Q);
+//     return 0;
+// }
+
+//19.write infix postfix
+#include <stdio.h>
 #include <stdlib.h>
-#define MAX 10
-void PUSH(int *stack, int *top){
-	if(*top != MAX-1){
-		int n;
-		scanf("%d",&n);
-		stack[++(*top)] = n;
-	}
-	else{
-		perror("\nMAX top size reached\n");
-	}
+#include <ctype.h>
+#include <string.h>
+
+int prec(char c) {
+    
+    if (c == '^')
+        return 3;
+    else if (c == '/' || c == '*')
+        return 2;
+    else if (c == '+' || c == '-')
+        return 1;
+    else
+        return -1;
 }
-void POP(int *stack,int *top){
-	if(*top != -1){
-		(*top)--;
-	}
-	else{
-		perror("\nStack is empty\n");
-	}
+
+void infixToPostfix(char* exp) {
+    int len = strlen(exp);
+    char result[len + 1];
+    char stack[len];
+    int j = 0;
+    int top = -1;
+
+    for (int i = 0; i < len; i++) {
+        char c = exp[i];
+
+     
+        if (isalnum(c)){
+            result[j++] = c;
+        }
+        else if (c == '(')
+            stack[++top] = '(';
+
+        
+        else if (c == ')') {
+            while (top != -1 && stack[top] != '(') {
+                result[j++] = stack[top--];
+            }
+            top--; 
+        }
+
+        
+        else {
+            while (top != -1 && (prec(c) < prec(stack[top]) ||
+                                 prec(c) == prec(stack[top]))) {
+                result[j++] = stack[top--];
+            }
+            stack[++top] = c;
+        }
+    }
+
+  
+    while (top != -1) {
+        result[j++] = stack[top--];
+    }
+
+    result[j] = '\0';
+    printf("%s\n", result);
 }
-void display(int stack[] ,int top){
-	printf("\n");
-	while(top!=-1){
-		printf("%d\n",stack[top]);
-		top--;
-	}
-	printf("\n\n");
-}
-void peek(int stack[],int top){
-	printf("\n%d\n\n",stack[top]);
-}
-int main(){
-	int top = -1;
-	char ch;
-	int stack[MAX];
-	int choice;
-	while (1){
-		printf("1.PUSH\n2.POP\n3.display\n4.peek\n5.exit\n");
-		printf("Enter a choice : ");
-		scanf("%d",&choice);
-		switch (choice)
-		{
-		case 1:
-			PUSH(stack,&top);
-			break;
-		case 2:
-			POP(stack,&top);
-			break;
-		case 3:
-			display(stack,top);
-			break;
-		case 4:
-			peek(stack,top);
-			break;
-		case 5:
-			exit(0);
-			break;
-		default:
-			printf("Invalid input");
-			break;
-		}
-		while (getchar() != '\n') {}
-		printf("\npress enter to continue.................. ");
-		while (getchar() != '\n') {}
-		system("clear");
-	}
-	return 0;
+
+int main() {
+    char exp[] = "a+b*c";
+    infixToPostfix(exp);
+    return 0;
 }
